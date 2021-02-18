d4d8e7b7b8cbb2378d4f122a394bc14ac8a5ef1c
John Bushnell <jbushnell@kochava.com>
2021-02-18 10:21:26 -0800

    4.0.0

---
+++ libKochavaUIiOS/KochavaUI.h
@@ -0,0 +1,25 @@
+//
+//  KochavaUI.h
+//  KochavaUI
+//
+//  Created by John Bushnell on 9/21/20.
+//  Copyright © 2020 Kochava, Inc.  All rights reserved.
+//
+
+
+
+#ifndef KochavaUI_h
+#define KochavaUI_h
+
+
+
+#import "KochavaCore.h"
+
+#import "KVAUIProduct.h"
+
+
+
+#endif
+
+
+
